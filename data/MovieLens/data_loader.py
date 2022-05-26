257f3c46fef3d060922c61fd42c2144d9af1b055
aDobeZ <johnnyzhang0611@gmail.com>
2022-05-25 20:48:29 -0700

    fix index problem in graph build

--- data/MovieLens/data_loader.py
+++ data/MovieLens/data_loader.py
@@ -3,27 +3,38 @@ import os
 import numpy as np
 import torch as th
 
-def parse_edge_index_file(filename):
+index_min = {}
+
+def parse_edge_index_file(src_name, dst_name, filename):
     """Parse edge index file."""
     with open(filename, 'r') as file_to_read:
         reader = file_to_read.read().splitlines()
         src = []
         dst = []
+    line = reader[1]
+    str_temp = line.strip().split("\t")
+    if src_name not in index_min:
+        index_min[src_name] = int(str_temp[1].strip())
+    if dst_name not in index_min:
+        index_min[dst_name] = int(str_temp[2].strip())
+    src_min = index_min[src_name]
+    dst_min = index_min[dst_name]
     for index in range(3, len(reader)):
         line = reader[index]
         str_temp = line.strip().split("\t")
-        row_index = int(str_temp[0].strip())
-        col_index = int(str_temp[1].strip())
+        row_index = int(str_temp[0].strip()) - src_min
+        col_index = int(str_temp[1].strip()) - dst_min
         src.append(row_index)
         dst.append(col_index)
     return src, dst
 
 
-def parse_label_index_file(filename):
+def parse_label_index_file(name, filename):
     """Parse label index file."""
     with open(filename, 'r') as file_to_read:
         reader = file_to_read.read().splitlines()
-    node_index = []
+    origin_index = []
+    new_index = []
     label = []
     label_list = []
     label_list_new = []
@@ -33,7 +44,8 @@ def parse_label_index_file(filename):
         line = reader[index]
         str_temp = line.strip().split("\t")
         curr_index = int(str_temp[0].strip())
-        node_index.append(curr_index)
+        origin_index.append(curr_index)
+        new_index.append(curr_index - index_min[name])
         max_idx = max(max_idx, curr_index)
         label_temp = int(str_temp[1].strip())
         label.append(label_temp)
@@ -47,17 +59,19 @@ def parse_label_index_file(filename):
         class_label = label[index]
         idx = label_list.index(class_label)
         class_label_new = label_list_new[idx]
-        label_array[node_index[index]] = class_label_new
-    return node_index, label_array, class_num
+        label_array[new_index[index]] = class_label_new
+    return origin_index, new_index, label_array, class_num
 
-def parse_set_split_file(filename):
+def parse_set_split_file(name, filename):
     np_indices = np.loadtxt(filename)
     """
     np_indices: numpy.array
     """
     indices = []
     for index in np_indices:
-        indices.append(th.from_numpy(index.astype(int)))
+        temp = np.ones(index.shape) * index_min[name]
+        new_index = index - temp
+        indices.append(th.from_numpy(new_index.astype(int)))
     return indices
 
 def process_movielens(root_path):
@@ -92,16 +106,16 @@ def process_movielens(root_path):
 
     #Construct graph from raw data.
     # movie_director
-    movie_director_src, movie_director_dst = parse_edge_index_file(os.path.join(data_path, 'movie_director.txt'))
+    movie_director_src, movie_director_dst = parse_edge_index_file('movie', 'director', os.path.join(data_path, 'movie_director.txt'))
 
     # user_movie
-    user_movie_src, user_movie_dst = parse_edge_index_file(os.path.join(data_path, 'user_movie_rating.txt'))
+    user_movie_src, user_movie_dst = parse_edge_index_file('user', 'movie', os.path.join(data_path, 'user_movie_rating.txt'))
 
     # movie_tag
-    movie_tag_src, movie_tag_dst = parse_edge_index_file(os.path.join(data_path, 'movie_tag.txt'))
+    movie_tag_src, movie_tag_dst = parse_edge_index_file('movie', 'tag', os.path.join(data_path, 'movie_tag.txt'))
 
     # movie_writer
-    movie_writer_src, movie_writer_dst = parse_edge_index_file(os.path.join(data_path, 'movie_writer.txt'))
+    movie_writer_src, movie_writer_dst = parse_edge_index_file('movie', 'writer', os.path.join(data_path, 'movie_writer.txt'))
 
     #build graph
     hg = dgl.heterograph({
@@ -117,9 +131,9 @@ def process_movielens(root_path):
     print("Graph constructed.")
 
     # Split data into train/eval/test
-    all_y_index, all_y_label, class_num = \
-            parse_label_index_file(os.path.join(data_path, 'movie_genre.txt'))
-    train_y_index = parse_set_split_file(os.path.join(data_path, 'movie_genre_train_idx.txt'))
-    test_y_index = parse_set_split_file(os.path.join(data_path, 'movie_genre_test_idx.txt'))
+    all_y_index, all_y_new_index, all_y_label, class_num = \
+            parse_label_index_file('movie', os.path.join(data_path, 'movie_genre.txt'))
+    train_y_index = parse_set_split_file('movie', os.path.join(data_path, 'movie_genre_train_idx.txt'))
+    test_y_index = parse_set_split_file('movie', os.path.join(data_path, 'movie_genre_test_idx.txt'))
 
     return hg, all_y_index, all_y_label, train_y_index, test_y_index, class_num
