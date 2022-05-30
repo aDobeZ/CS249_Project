0487fc4414fa2e89ce1504e355bf06fbbbaa66ea
xinyu <xinyu9611work@gmail.com>
2022-05-29 21:32:51 -0700

    change RGCN baseline

--- rgcn/main.py
+++ rgcn/main.py
@@ -46,19 +46,19 @@ def main(args):
 		category = "movie"
 		dataset = 'MovieLens'
 		min_index = 0
-		batch = 20
+		batch = args.batch
 	elif args.dataset == 'cora':
 		dataloader = cora_loader
 		category = "paper"
 		dataset = 'Cora'
 		min_index = 24961
-		batch = 80
+		batch = args.batch
 	elif args.dataset == 'dblp':
 	    dataloader = dblp_loader
 	    category = "author"
 	    dataset = 'DBLP_four_area'
 	    min_index = 0
-	    batch = 80
+	    batch = args.batch
 	else:
 		raise ValueError()
 
@@ -66,7 +66,6 @@ def main(args):
 	g, all_y_index, all_y_label, train_y_index, test_y_index, num_classes = dataloader(DATA_PATH)
 	pool_index = np.array(train_y_index[0])
 	num_train_nodes = len(pool_index)
-	print(num_train_nodes)
 	num_pool_nodes = int(num_train_nodes / 2)
 	# train_idx = th.from_numpy(pool_index[0:num_pool_nodes])
 	val_idx = th.from_numpy(pool_index[num_pool_nodes:num_train_nodes])
@@ -85,10 +84,7 @@ def main(args):
 	print("adj_shape:\t", old_adj.shape)
 
 	# preprocess train index
-	maxIter = int(num_pool_nodes / batch)
-	if maxIter > 40: 
-		maxIter = 40
-		maxIter = 1
+	maxIter = args.iteration
 
 	# define parameters
 	outs_train = []
@@ -146,8 +142,8 @@ def main(args):
 	# 	baseline_record = np.concatenate((baseline_record, np.array(base_record)), axis=0)
 	# 	best_baseline.append(base_best)
 	idx = [i for i in range(record.shape[0])]
-	# plot_figure(idx, record, [0, 2, 4], "Accuracy")
-	# plot_figure(idx, record, [1, 3, 5], "Loss")
+	plot_figure(idx, record, [0, 2, 4], "Accuracy")
+	plot_figure(idx, record, [1, 3, 5], "Loss")
 	print("ActiveRGCN record shape:\t\t", record.shape)
 	print("RGCNbaseline record shape:\t", np.array(baseline_record).shape)
 	print("best active:\n", np.array(best_active))
@@ -178,6 +174,10 @@ if __name__ == '__main__':
 	parser.add_argument("--use-self-loop", default=False, action='store_true',
 			help="include self feature as a special relation")
 	parser.add_argument("--set", type=str, default='ori', help="setting of score function")
+	parser.add_argument("--batch", type=int, default=20,
+			help="set batch size")
+	parser.add_argument("--iteration", type=int, default=40,
+			help="set iteration number")
 	fp = parser.add_mutually_exclusive_group(required=False)
 	fp.add_argument('--validation', dest='validation', action='store_true')
 	fp.add_argument('--testing', dest='validation', action='store_false')
