40518ff68b0e9b321a2310222e7161e3b540411e
Haowei Jiang <haowei98@uw.edu>
2022-05-29 23:31:08 -0700

    add three experiments running script

---
+++ run_movielens_hyperparam.sh
@@ -0,0 +1,16 @@
+: '
+HyperParameter testing on Movielens Dataset
+BATCH * ITR = 800
+20 * 40
+50 * 16
+80 * 10
+100 * 8
+160 * 5
+200 * 4
+'
+for i in 20 50 80 100 160 200; do
+    # echo "$i"
+    itr=$((800 / $i))
+    # echo "$itr"
+    python ./rgcn/main.py -d movielens --testing --gpu 0 --set ori --batch $i --iteration $itr > "./hyperparam_experiment/movielens_al_batch_${i}_itr_${itr}.txt"
+done
